5f23bb52f3eed727fd197bf5cec4fc8d33cf9fc3
Christian Rogobete <c.rogobete@soneso.com>
2022-05-26 20:42:33 +0200

    fix HorizonErrorResponseExtras Json decoding

--- Soneso/StellarSDK/Responses/Errors/HorizonErrorResponseExtras.php
+++ Soneso/StellarSDK/Responses/Errors/HorizonErrorResponseExtras.php
@@ -49,17 +49,20 @@ class HorizonErrorResponseExtras
     {
         return $this->resultCodesOperation;
     }
-
+    
     protected function loadFromJson(array $json): void
     {
         if (isset($json['envelope_xdr'])) $this->envelopeXdr = $json['envelope_xdr'];
         if (isset($json['result_xdr'])) $this->resultXdr = $json['result_xdr'];
-        if (isset($json['result_codes.transaction'])) $this->resultCodesTransaction = $json['result_codes.transaction'];
+        if (isset($json['result_codes'])) {
 
-        $this->resultCodesOperation = array();
-        if (isset($json['result_codes']['operations'])) {
-            foreach ($json['result_codes']['operations'] as $resultCode) {
-                $this->resultCodesOperation[] = $resultCode;
+            if (isset($json['result_codes']['transaction'])) $this->resultCodesTransaction = $json['result_codes']['transaction'];
+
+            $this->resultCodesOperation = array();
+            if (isset($json['result_codes']['operations'])) {
+                foreach ($json['result_codes']['operations'] as $resultCode) {
+                    $this->resultCodesOperation[] = $resultCode;
+                }
             }
         }
     }
